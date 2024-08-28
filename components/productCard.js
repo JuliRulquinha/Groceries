9d707ea264c22cdeb47cf51436712886e393de58
JuliRulquinha <juli-felix@outlook.com>
2024-08-28 14:17:40 -0300

    Added a div to display the availability of the products as well as the quantity

--- components/productCard.js
+++ components/productCard.js
@@ -10,6 +10,8 @@ function createCard(product)
     const productImgElement = document.createElement("img");
     const priceContainerDiv = document.createElement("div");
     const ratingContainerDiv = document.createElement("div");
+    const quantityContainerDiv = document.createElement("div");
+    let isAvailableContainerDiv = document.createElement("div");
 
     mainDiv.appendChild(cardDiv);
     cardDiv.appendChild(nameContainerdiv);
@@ -17,6 +19,8 @@ function createCard(product)
     cardDiv.appendChild(textContainerDiv);
     cardDiv.appendChild(priceContainerDiv);
     cardDiv.appendChild(ratingContainerDiv);
+    cardDiv.appendChild(quantityContainerDiv);
+    cardDiv.appendChild(isAvailableContainerDiv);
     
     
     cardDiv.className = "card-div";
@@ -25,6 +29,8 @@ function createCard(product)
     nameContainerdiv.className = "name-container";
     priceContainerDiv.className = "price-container";
     ratingContainerDiv.className = "rating-container";
+    quantityContainerDiv.className = "quantity-container";
+    isAvailableContainerDiv.className = "availability-check";
 
 
     
@@ -38,6 +44,19 @@ function createCard(product)
     paragraphElement.innerText = product.description;
     priceContainerDiv.innerText = "R$ "+product.price;
     textContainerDiv.appendChild(paragraphElement); 
+    isAvailableContainerDiv = product.isAvailable;
+    
+    if(product.quantity > 0)
+    {
+        product.isAvailable = true;
+        quantityContainerDiv.innerText = product.quantity+" in stock";
+    
+    }
+    else
+    {
+        product.isAvailable = false;
+        quantityContainerDiv.innerText = "This product is not available at the moment";
+    }
     
 
    
