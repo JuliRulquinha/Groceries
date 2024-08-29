b1f30ca46b89e0241de9f206ab4b8b0dbe6f079b
JuliRulquinha <juli-felix@outlook.com>
2024-08-29 14:04:33 -0300

    Added comments inside the function for each section, to explain what is being done

--- components/productCard.js
+++ components/productCard.js
@@ -1,6 +1,8 @@
 
 function createCard(product)
 {
+    //Declaration section.
+
     const cardDiv = document.createElement("div");
     const imageContainerDiv = document.createElement("div");
     const textContainerDiv = document.createElement("div");
@@ -14,6 +16,8 @@ function createCard(product)
     let isAvailableContainerDiv = document.createElement("div");
     const anchorPageRedirect = document.createElement("a");
 
+    //Appending section.
+
     mainDiv.appendChild(cardDiv);
     cardDiv.appendChild(nameContainerdiv);
     cardDiv.appendChild(imageContainerDiv);
@@ -23,7 +27,8 @@ function createCard(product)
     cardDiv.appendChild(isAvailableContainerDiv);
     cardDiv.appendChild(ratingContainerDiv);
     
-    
+    //Class attribution section.
+
     cardDiv.className = "card-div";
     imageContainerDiv.className = "image-container";
     textContainerDiv.className = "text-container";
@@ -34,8 +39,8 @@ function createCard(product)
     isAvailableContainerDiv.className = "availability-check";
     anchorPageRedirect.className = "product-anchor";
 
+    //This section is affecting how the images are displayed and its behavior.
 
-    
     productImgElement.src = product.imgUrl;
     productImgElement.setAttribute("width","100%");
     productImgElement.setAttribute("height","100%");
@@ -44,6 +49,12 @@ function createCard(product)
     anchorPageRedirect.appendChild(productImgElement);
     imageContainerDiv.appendChild(anchorPageRedirect);
 
+    /*This section is used to assign each object property to the inner text 
+    of the corresponding element. As well as symbols and a boolean variable
+    to evaluate weather the products are available or not.*/
+
+    /*TO DO: Add a behavior to the star rating container, so when the user hovers
+    over the stars */ 
 
     nameContainerdiv.innerText = product.name;
     paragraphElement.innerText = product.description;
@@ -52,12 +63,13 @@ function createCard(product)
     isAvailableContainerDiv = product.isAvailable;
     ratingContainerDiv.innerText = "☆ ☆ ☆ ☆ ☆";
     
-    
+    /*"If" statement declared to test if the variable "isAvailable" is true based 
+    on the quantity of products in stock */
+
     if(product.quantity > 0)
     {
         product.isAvailable = true;
         quantityContainerDiv.innerText = product.quantity+" in stock";
-    
     }
     else
     {
