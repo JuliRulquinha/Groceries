b68fe1ff76b24f9a20e6ef6aedc932a744332638
JuliRulquinha <juli-felix@outlook.com>
2024-08-30 16:43:13 -0300

    Added a footer to the page

--- Index.js
+++ Index.js
@@ -24,17 +24,17 @@ function createProduct(name, imgUrl, description,price, quantity,isAvailable)
     }
 }
 
-let productBasket = [createProduct("Apricot",`https://cdn.britannica.com/36/160636-050-B1DC5C0A/Laetrile-apricot-pits.jpg`,
+let productBasket = [createProduct("APRICOT",`https://cdn.britannica.com/36/160636-050-B1DC5C0A/Laetrile-apricot-pits.jpg`,
     `Similar in shape to a peach, the product is nearly smooth, 
     round to oblong in some varieties, 
     and somewhat flattened but with little 
     to no hairiness when ripe. Its flesh is 
     typically a rich yellow to yellowish orange.`, 23.99, 200, true), 
-    createProduct("Banana","https://static.libertyprim.com/files/familles/banane-large.jpg?1569271725",`Bananas are long, curved products with smooth skin 
+    createProduct("BANANA","https://static.libertyprim.com/files/familles/banane-large.jpg?1569271725",`Bananas are long, curved products with smooth skin 
     that are usually yellow, but can also be green, red, 
     or purple depending on the variety. 
     They are about 7 to 9 inches long and 2 to 3 inches in diameter.`, 7.99, 5000, true), 
-    createProduct("Strawberry",`https://www.collinsdictionary.com/images/full/strawberry_227472010.jpg`
+    createProduct("STRAWBERRY",`https://www.collinsdictionary.com/images/full/strawberry_227472010.jpg`
     ,`Strawberries are soft, sweet, and juicy red berries that grow on low-flowering plants. 
     They have tiny edible seeds that cover their surface and are 
     the enlarged ends of the plant's stamen.`,10.99, 1000, true)];
