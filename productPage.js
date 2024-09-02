d740bf2579c575c162ba5389054703917bca6107
JuliRulquinha <juli-felix@outlook.com>
2024-09-02 17:59:43 -0300

    Learning about queryString

--- productPage.js
+++ productPage.js
@@ -2,5 +2,35 @@
 function createMainPage(){
     
     const mainDiv = document.getElementById("main-div");
-    const 
+     
 }
+
+//TODO Read the querystring
+var querystring = window.location.search;
+var urlParameters = new URLSearchParams(querystring);
+
+// retrieve the id property
+let idParam = urlParameters.get("id");
+
+
+let products = [];
+let product = {};
+
+fetch("productBasket.json")
+.then(response => response.json())
+.then(productsReceived => {
+    products = productsReceived;         
+    // use the id property to obtain the corresponding object from products array
+    product = products.filter(p => p.id == idParam);   
+    
+    
+    console.log(product[0]);
+    //" draw "  the product on the screen
+    
+});
+
+
+
+
+
+
